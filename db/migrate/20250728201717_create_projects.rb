5524e1172665618cd1a0f3f0ef28028b8981a857
Amanawl <amanawlachew775@gmain.com>
2025-07-28 23:17:50 +0300

    create project  model

---
+++ db/migrate/20250728201717_create_projects.rb
@@ -0,0 +1,15 @@
+class CreateProjects < ActiveRecord::Migration[8.0]
+  def change
+    create_table :projects do |t|
+      t.string :title
+      t.text :description
+      t.date :start_date
+      t.date :end_date
+      t.integer :status
+      t.integer :priority
+      t.references :owner, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
